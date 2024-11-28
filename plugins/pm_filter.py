3705714cd1fb695a1e0e1d132b91889bc47a132e
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-28 18:51:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -32,7 +32,11 @@ BUTTONS2 = {}
 SPELL_CHECK = {}
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
-async def give_filter(client, message):
+async def give_filter(client, message)
+    try:
+        await message.react(emoji=random.choice(REACTIONS), big=True)
+    except:
+        pass
     if message.chat.id != SUPPORT_CHAT_ID:
         settings = await get_settings(message.chat.id)
         chatid = message.chat.id 
@@ -74,6 +78,10 @@ async def give_filter(client, message):
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
+    try:
+        await message.react(emoji=random.choice(REACTIONS), big=True)
+    except:
+        pass
     content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
