614b51bd0487468dbd847f14a51a2fb6ebf2967c
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-29 14:52:28 +0530

    Update broadcast.py

--- CloneBot/broadcast.py
+++ CloneBot/broadcast.py
@@ -54,9 +54,9 @@ async def pm_broadcast(bot, message):
     except Exception as e:
         print(f"error: {e}")
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @tamilBots
+# Subscribe YouTube Channel For Amazing Bot @TamilBots
+# Ask Doubt on telegram @TamilSupport
 
 async def broadcast_messages(bot_id, user_id, message):
     try:
