22b73de2c1ded422cb0c65e02d3207cd23613df4
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-29 15:35:45 +0530

    Update cb_data.py

--- plugins/Extra/rename/cb_data.py
+++ plugins/Extra/rename/cb_data.py
@@ -1,6 +1,7 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @TamilBots
+# Subscribe YouTube Channel For Amazing Bot @TamilBots
+# Ask Doubt on telegram @TamilSupport
+#Join our Movie channel @TownBus
 
 from plugins.Extra.utils import progress_for_pyrogram, convert, humanbytes
 from pyrogram import Client, filters
